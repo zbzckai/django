a53be56f43bd6a9f86a9ab2a1cf735d47a16216a
zbzckai <1132382311@qq.com>
2019-02-28 07:45:06 +0800

    ss

--- wangye/js第一天/js/test.js
+++ wangye/js第一天/js/test.js
@@ -1,12 +1,9 @@
-console.log("woshiwqamian de ")
-// // switch (表达式){
-// 	case 1:
-// 	语句
-// 	case 2:
-// 	语句
-// break；
-// 	...
-// 	default
-// }
-
-// 匹配到后会继续向下执行,如果只想让他执行改行语句,要加break
+var arr1 = new Array()
+console.log(arr1)
+var arr1 = new Array(1,2,3,"sas")
+// 可以存不同的数据
+console.log(arr1)
+var arr1 = new Array(5)
+// 如果只有一个参数则是显示的建议的元素个数，但是可以有5个
+arr1 = [1,2,3,4,5,6]
+console.log(arr1)
