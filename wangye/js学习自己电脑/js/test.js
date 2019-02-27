48eb33bb9fcb514b74e84a8dc44002f5e15a5677
zbzckai <1132382311@qq.com>
2019-02-27 20:32:39 +0800

    ss

--- wangye/js学习自己电脑/js/test.js
+++ wangye/js学习自己电脑/js/test.js
@@ -1,3 +1,21 @@
-console.log("<h1>sunck</h1>")
-alert("<h1>sunck</h1>")
-document.write("<h1>sunck</h1>")
+var arr = new Array()
+arr[0] = 1
+arr[1] = 2
+arr[2] = 3
+arr[3] = 4
+arr.forEach(function(item){
+	console.log(item)
+})
+//加到最后一项
+arr.push(6)
+console.log(arr)
+//加到第一项
+arr.unshift(7)
+console.log(arr)
+//拿数据默认最后一个
+arr.pop(3)
+
+//取数据默认第一个
+arr.shift(2)
+console.log(arr.reverse())
+console.log(arr.slice(1,3))
