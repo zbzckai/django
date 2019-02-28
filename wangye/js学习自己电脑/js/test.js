06236f4efe3ce6c89a609fb77aa31544170bf966
zbzckai <1132382311@qq.com>
2019-02-28 17:01:27 +0800

    s

--- wangye/js学习自己电脑/js/test.js
+++ wangye/js学习自己电脑/js/test.js
@@ -1,21 +1,8 @@
-var arr = new Array()
-arr[0] = 1
-arr[1] = 2
-arr[2] = 3
-arr[3] = 4
-arr.forEach(function(item){
-	console.log(item)
-})
-//加到最后一项
-arr.push(6)
-console.log(arr)
-//加到第一项
-arr.unshift(7)
-console.log(arr)
-//拿数据默认最后一个
-arr.pop(3)
+var str1 = "sunck is a nice man"
+var str3 = str1.replace("nice","good")
+console.log(str3)
+console.log(str1.substring(9,13))
+console.log(str1.substr(1,20))
 
-//取数据默认第一个
-arr.shift(2)
-console.log(arr.reverse())
-console.log(arr.slice(1,3))
+
+console.log(parseInt(Math.random()*(6-2+1)+2))
---
+++ wangye/js联系/js/test.js
@@ -0,0 +1,8 @@
+// 在当前页面打开一个新窗口
+function func(){
+	window.open("red.html","blank","width = 200px ,height = 200px,top = 20px")
+}
+// 关闭当前页面
+function func1(){
+	window.close()
+	}
